83c3d4edaffe24b4f7fde40e167559fbbee9dd0a
tarfaj24 <tarfajozef@gmail.com>
2026-02-05 12:59:07 +0100

    sanoo

--- uloha.php
+++ uloha.php
@@ -12,14 +12,17 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     echo "</table>";
 }
 ?>
+<!-- šANO -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+    <p></p>
     <form method="post">
         <div>
             <label for="riadok">Riadkov</label>
