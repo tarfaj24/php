c18d9e893f3ce98692d857da9758cffc6a45fecf
Jozef Tarfa <tarfajozef@gmail.com>
2026-03-18 20:08:18 +0100

    add-komunikacia_s_databazou

--- databazy_a_objekty/Kniha.php
+++ databazy_a_objekty/Kniha.php
@@ -1,40 +1,41 @@
 <?php
 
-class Kniha{
-    public string $nazov;
-    public string $autor;
-    public int $rok_vydania;
-    public int $stav;
+class Kniha
+{
+    private int $id;
+    private string $nazov;
+    private string $autor;
+    private int $rok_vydania;
 
-    public function __construct($nazov,$autor,$rok_vydania,$stav){
+    private int $stav;
+
+    public function __construct($nazov, $autor, $rok_vydania, $stav){
         $this->nazov = $nazov;
         $this->autor = $autor;
         $this->rok_vydania = $rok_vydania;
         $this->stav = $stav;
-
     }
+
+
     public function getNazov(){
         return $this->nazov;
     }
+    public function setId($id){
+        $this->id = $id;
+    }
 
-
-    public function get_Autor(){
+    public function getId(){
+        return $this->id;
+    }
+    public function getAutor(){
         return $this->autor;
     }
 
-
-    public function get_Rok_vydania(){
+    public function getRok_vydania(){
         return $this->rok_vydania;
-
-
     }
 
-    public function get_Stav(){
+    public function getStav(){
         return $this->stav;
     }
-
-    
-
 }
-
-?>
--- databazy_a_objekty/index.php
+++ databazy_a_objekty/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 include "Kniha.php";
 include "Database.php";
@@ -6,26 +6,40 @@ include "Database.php";
 $spojenie = new Database();
 $db = $spojenie->nadviazSpojenie();
 
-echo "<br>";
+
+if(!$db){
+    die(" Databaza nie je pripojena");
+}
 
 
-var_dump($db);
+$sql = "SELECT * FROM knihy";
+$stmt = $db->query($sql);
+
+if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["action"]) && $_POST["action"] === "delete"){
+    $sql = "DELETE FROM knihy WHERE id = :id";
+
+    $stmt = $db->prepare($sql);
+
+    return $stmt->execute([
+        ":id" => $_POST["kniha_id"]
+    ]);
+
+    header("Location:index.php");
+    exit();
+}
 
 
-$kniznica = [];
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $kniha = null;
 
-$svet = new Kniha("Svet","Janko Hruška",1897,1);
-$kniznica[] = $svet;
+    $kniha = new Kniha($row["nazov"], $row["autor"], (int)$row["rok_vydania"], (int)$row["stav"]);
 
-$auto = new Kniha("Auto","Stano Mak",2000,1);
-$kniznica[] = $auto;
+    if($kniha){
+        $kniha->setId($row["id"]);
+        $kniznica[] = $kniha;
+    }
+};
 
-
-
-// echo $svet->getNazov()."<br>";
-// echo $svet->get_Autor()."<br>";
-// echo $svet->get_Rok_vydania()."<br>";
-// echo $svet->get_Stav()."<br>";
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +47,8 @@ $kniznica[] = $auto;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabulka</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
 </head>
 <body>
     <h1>Kniznica</h1>
@@ -44,31 +59,32 @@ $kniznica[] = $auto;
             <th>Autor</th>
             <th>Rok vydania</th>
             <th>Stav</th>
+            <th>Action</th>
         </tr>
 
-        
-        
-            <?php foreach($kniznica as $kniha):?> 
-                <tr>
-                    <td>
-                        <?= $kniha->getNazov() ?>
-                    </td>
-                    <td>
-                        <?= $kniha->get_Autor() ?>
-                    </td>
-                    <td>
-                        <?= $kniha->get_Rok_vydania() ?>
-                    </td>
-                    <td>
-                        <?= $kniha->get_Stav() ?>
-                    </td>
-                </tr>
-                 
-            <?php endforeach;?>
-
-        
-        
+        <?php foreach($kniznica as $kniha):?>
+            <tr>
+                <td>
+                    <?= $kniha->getNazov() ?>
+                </td>
+                <td>
+                    <?= $kniha->getAutor() ?>
+                </td>
+                <td>
+                    <?= $kniha->getRok_vydania() ?>
+                </td>
+                <td>
+                    <?= $kniha->getStav() ?>
+                </td>
+                <td>
+                    <form action="index.php" method="POST">
+                        <input type="hidden" name="action" value="delete">
+                        <input type="hidden" name="kniha_id" value="<?php $kniha->getId();?>">
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>                
+            </tr>
+        <?php endforeach; ?>
     </table>
-    
 </body>
 </html>
